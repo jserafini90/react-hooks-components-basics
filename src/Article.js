7ea954771b2a2b5781cada14e3a0bb19781b6ee3
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-09-01 17:09:49 -0400

    commit tree 9fd79d0b0fc7115630af603e9250e2e24d674504

--- src/Article.js
+++ src/Article.js
@@ -6,4 +6,17 @@ function Article() {
   );
 }
 
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Article />
+      <Comment />
+    </div>
+  );
+}
+
 export default Article;
